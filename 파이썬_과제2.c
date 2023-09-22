312663a46c277d04820800698d864afc3435da3a
Skysny12 <127105223+Skysny12@users.noreply.github.com>
2023-09-22 18:46:22 +0900

    Add files via upload

---
+++ 파이썬_과제2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int mainfuction(void) {
+
+	double kilo;
+	double mile;
+	printf("Please enter kilometers:");
+	scanf_s("%lf", &kilo);
+	mile = (1 / 1.609) * kilo;
+	printf("%.1f km is equal to %.1f miles.", kilo, mile);
+	return 0;
+}
